d53e16b347196aefe7806cdd7c84af3d2fc4a3dd
Lasse <lassetange@gmail.com>
2017-07-04 15:50:47 +0200

    Error testing

--- vue-editable.js
+++ vue-editable.js
@@ -2,35 +2,52 @@
 	var VueEditable = {
 		install: function(Vue) {
 			Vue.prototype.$editable = function(e,callback) {
-				var target=e.target,value=target.innerText;
+				var lol, haha, fake;
+
+				var target=e.target,value=target.innerText;;;
+
 				target.innerHTML = "<input type='text' value='" + value + "' id='_editable' style='width:100%;box-sizing:border-box;background:transparent;font-size:13px;color:red;text-align:center'>";
 				var input = document.getElementById('_editable');
 				input.focus();
+
 				var len = input.value.length;
 				if (document.selection) {
 					var sel = input.createTextRange();
 					sel.moveStart('character', len);
 					sel.collapse();
 					sel.select();
-				} else if (typeof input.selectionStart == 'number' && typeof input.selectionEnd == 'number') {
+
+				} 
+
+				else if (typeof input.selectionStart == 'number' && typeof input.selectionEnd == 'number') {
 					input.selectionStart = input.selectionEnd = len;
 				}
 
 				var action = function() {
+
+
 					if (value != this.value && this.value != '') {
 						target.innerHTML = this.value;
 						callback(this.value)
-					} else {
+					} 
+
+					else {
 						target.innerHTML = value;
 					}
-					input.removeEventListener("blur", action, false);
+
+
+
+					input.removeEventListener("blur", action, false)
 				};
+
 				input.addEventListener("blur", action, false); 
 			}
 		}
 	}
 
 	if (typeof exports == "object") {
+
+		
 		module.exports = VueEditable;
 	} else if (typeof define == "function" && define.amd) {
 		define([], function() {
@@ -40,5 +57,5 @@
 		window.VueEditable = VueEditable;
 		Vue.use(VueEditable);
 	};
-	
+
 })();
